e6c35ce89cb0071333b1f1a57484056c9b0efa46
Alan Dri <alandri33@gmail.com>
2023-01-05 18:02:07 -0300

    arreglo fondo animado

--- js/main.js
+++ js/main.js
@@ -16,19 +16,44 @@ btn.addEventListener('click', () => {
     cambiarAModoDark(); 
 });
 
+window.addEventListener("resize", ()=> {
+    if(window.innerWidth >= 500) {
+        document.getElementById('nav-letters').style.display = 'inline-block';
+        document.getElementById('menu-icon').style.display = 'none';
+    }
+});
+
+window.addEventListener("resize", ocultarNav);
+
+window.addEventListener("resize", moverElemento);
+
+moverElemento();
+
 function ocultarNav () {
-    if(screen.width < 501) {
+    if(window.innerWidth < 500) {
         document.getElementById('nav-letters').style.display = 'none';
         document.getElementById('menu-icon').style.display =  "flex";
     }
 }
 
 function mostrarNav() {
-    document.getElementById('nav-letters').style.display = 'block';
+    document.getElementById('nav-letters').style.display = 'inline-block';
     document.getElementById('menu-icon').style.display = 'none';
 }
 
 function cambiarAModoDark() {
     document.body.classList.toggle('dark');
     btn.classList.toggle('active');
+}
+
+function moverElemento() {
+    if (window.innerWidth < 500) {
+        let elemento = document.getElementById("p-estudio");
+        let destino = document.getElementById("icon-estudio");
+        destino.parentNode.insertBefore(elemento, destino);
+    } else {
+        let elemento = document.getElementById("icon-estudio");
+        let destino = document.getElementById("p-estudio");
+        destino.parentNode.insertBefore(elemento, destino);
+    }
 }
